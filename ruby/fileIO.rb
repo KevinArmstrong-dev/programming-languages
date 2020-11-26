95f15b2f096b0d4bf4fdfbff36fb5c491908b82a
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2020-11-26 17:42:35 -0500

    Changed regex

--- ruby/fileIO.rb
+++ ruby/fileIO.rb
@@ -19,17 +19,18 @@ class FileIO
         @inputList = input.split(/[\s,']/)
         # puts @inputList
         case @inputList[0]
-        when /shape/
+        when /\shape/
             @shape = Shape.new()
             puts "Shape, Perimeter: #{@shape.perimeter}, area: #{@shape.area}"
-        when /[rectangle]/
+        when /\rectangle/
             puts "Found rectangle"
             rectangle = Rectangle.new(@inputList[1],@inputList[2])
             puts "Rectangle, Perimeter: #{@rectangle.perimeter}, area: #{@rectangle.area} "
-        when /[circle]/
+        when /^circle/
+            puts "In circle !!!"
             circle = Circle.new(@inputList[1])
             puts "Circle, Perimeter: #{@circle.perimeter}, area: #{@circle.area} "
-        when /[ellipse]/
+        when /^ellipse/
             ellipse = Ellipse.new(@inputList[1],@inputList[2])
             puts "Ellipse, Perimeter: #{@ellipse.perimeter}, area: #{@ellipse.area} "
         else
